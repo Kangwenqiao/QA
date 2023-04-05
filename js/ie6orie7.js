f23b0c211e0e1eafdf6167dd3693cd64f92cf993
Kang <118408678+Kangwenqiao@users.noreply.github.com>
2023-04-05 11:32:52 +0800

    Delete js directory

--- js/ie6orie7.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    if(navigator.appName == "Microsoft Internet Explorer" &&(navigator.appVersion .split(";")[1].replace(/[ ]/g,"")=="MSIE6.0"||navigator.appVersion .split(";")[1].replace(/[ ]/g,"")=="MSIE7.0") )
-    {
-        $('body').css({'background-color':'#3cbbdc'}).html('<div id="web-ietip"><a href="http://se.360.cn/" target="_blank" title="360娴忚鍣�" style="width: 60px;"></a><a href="http://chrome.360.cn/" target="_blank" title="360鏋侀€熸祻瑙堝櫒" style="width: 53px;"></a><a href="http://ie.sogou.com/" target="_blank" title="鎼滅嫍娴忚鍣�"></a><a href="http://browser.qq.com/" target="_blank" title="QQ娴忚鍣�" style="width: 55px;"></a><a href="http://www.liebao.cn/" target="_blank" title="鐚庤惫娴忚鍣�" style="width: 55px;"></a><a href="http://www.google.cn/intl/zh-CN/chrome/browser/" target="_blank" title="璋锋瓕娴忚鍣�"></a><a href="http://www.firefox.com.cn/" target="_blank" title="鐏嫄娴忚鍣�" style="width: 55px;"></a><a href="https://www.apple.com/cn/safari/" target="_blank" title="浠呮敮鎸丮ac绯荤粺" style="width: 58px;"></a><a href="http://rj.baidu.com/search/index/?kw=IE11" target="_blank" style="width: 55px;"></a></div>')
-    }
-});
-   
